c5253a54ffcf60289090a9086d74475ca064079c
LoptrSir <91218015+LoptrSir@users.noreply.github.com>
2023-10-11 14:40:54 -0600

    reset, playAgain function

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 //TTT the phoenix version from the ashes of spaghetti.
-
-// declare winner get name without another variable. create a playerFactory to reduce declared variables and simplify bits. turn off tile listeners upon win.
+// move tile listener into function and call at PlayGame. disable 'this round goes to' upon playAgain, reset: remove player names from input field and deselect x/o,
 
 //playerFactory
 const playerFactory = function (name, mark) {
@@ -12,10 +11,7 @@ let player1 = playerFactory("Player 1", "X");
 let player2 = playerFactory("Player 2", "O");
 let p1 = document.getElementById("p1");
 let p2 = document.getElementById("p2");
-//let player1Name; //migrate to pF
-//let player1Mark; //migrate to pF
-//let player2Name; //migrate to pF
-//let player2Mark; //migrate to pF
+let cat; //Tie game winner
 const playGameBtn = document.getElementById("playGame");
 let currentPlayer;
 let isPlayer1Turn = true;
@@ -23,15 +19,14 @@ let tile;
 const tiles = document.querySelectorAll(".tile");
 let playedTiles = [];
 let gameWon;
-let cat;
 const victory = document.getElementById("victory"); //Makes win statement visible
 const round = document.getElementById("round"); //Declares winner
 let p1Win = document.getElementById("p1win"); //DisplayScores
 let p2Win = document.getElementById("p2win"); //DisplayScores
 let catWin = document.getElementById("catwin"); //DisplayScores
-//let p1Score = 0; //migrate to pF
-//let p2Score = 0; //migrate to pF
 let catScore = 0;
+let reset = document.getElementById('reset');
+let playAgain = document.getElementById('playAgain');
 
 const winningConditions = [
   ["b1", "b2", "b3"],
@@ -44,21 +39,9 @@ const winningConditions = [
   ["b3", "b5", "b7"],
 ];
 
-//Display text module
+//Display text module  playGameBtn.addEventListener("click", displayText.displayNames);
 const displayText = (() => {
   const displayNames = () => {
-    // player1 = playerFactory(
-    //   document.querySelector('#player1 input[name="name"]').value,
-    //   document.querySelector('#player1 input[name="choice"]:checked').value
-    // );
-    // player2 = playerFactory(
-    //   document.querySelector('#player2 input[name="name"]').value,
-    //   player1.mark === null
-    //     ? alert("Player 1 select X or O")
-    //     : player1.mark === "X"
-    //     ? "O"
-    //     : "X"
-    // );
     player1.name = document.querySelector('#player1 input[name="name"]').value;
     player2.name = document.querySelector('#player2 input[name="name"]').value;
     player1.mark = document.querySelector(
@@ -72,6 +55,16 @@ const displayText = (() => {
         : "X";
     p1.textContent = `Player one: ${player1.name} is playing ${player1.mark} `;
     p2.textContent = `Player two: ${player2.name} is playing ${player2.mark}`;
+
+    playGameBtn.disabled = true;
+
+        document.querySelector('#player1 input[name="name"]').disabled = true;
+        const player1ChoiceInputs = document.querySelectorAll('#player1 input[name="choice"]');
+        player1ChoiceInputs.forEach(input => input.disabled = true);
+        document.querySelector('#player2 input[name="name"]').disabled = true;
+        const player2ChoiceInputs = document.querySelectorAll('#player2 input[name="choice"]');
+        player2ChoiceInputs.forEach(input => input.disabled = true);
+
     console.log(
       "displayNames:",
       player1.name,
@@ -90,126 +83,106 @@ const displayText = (() => {
     } else {
       currentPlayer.score++;
       victory.style.display = "block";
-      round.textContent = `This round goes to ${currentPlayer.name} playing ${currentPlayer.mark}.`;
+      round.textContent = `This round goes to ${currentPlayer.name}.`;
       //add remove listener here
     }
-      p1Win.textContent = `${player1.name}'s score is ${player1.score}.`;
-      p2Win.textContent = `${player2.name}'s  score is ${player2.score}.`;
-      catWin.textContent = `TACOCAT's score is ${catScore}.`;
-    
+    p1Win.textContent = `${player1.name}'s score is ${player1.score}.`;
+    p2Win.textContent = `${player2.name}'s  score is ${player2.score}.`;
+    catWin.textContent = `TACOCAT's score is ${catScore}.`;
   }
 
-  // const displayWin = () => { //How do I call currentPlayerName without another variable
-  //   //Update pXScore++; //How? this.pXScore?
-  //   victory.style.display = "block";
-  //   round.textContent = `This round goes to ${currentPlayer}.`; //Correct winner to proper winner IE: this.player or whatever
-  //   p1Win.textContent = `${player1Name}'s score is ${p1Score}.`;
-  //   p2Win.textContent = `${player2Name}'s  score is ${p2Score}.`;
-  //   catWin.textContent = `TACOCAT's score is ${catScore}.`;
-  // };
-  // const displayTie = () => {
-  //   catScore++;
-  //   victory.style.display = "block";
-  //   round.textContent = "This round goes to the TACOCAT.";
-  //   p1Win.textContent = `${player1Name}'s score is ${p1dScore}.`;
-  //   p2Win.textContent = `${player2Name}'s  score is ${p2dScore}.`;
-  //   catWin.textContent = `TACOCAT's score is ${catScore}.`;
-  // };
   return { displayNames, displayWin };
 })();
 
 //Game Logic
-//const gameLogic = (() => {  //Should the gameLogic be wrapped in a module or left open to the global?
-//   return { checkWin, checkTie, updateBoard, reset };
-//})();
-
-const handlePlayerMove = (event) => {
-  tile = event.target;
-  console.log("handlePlayerMove players:", player1, player2);
-  if (!tile.textContent && !playedTiles.includes(tile)) {
-    // if (isPlayer1Turn && player1) {
-    //     currentPlayer = player1;
-    //   } else if (!isPlayer1Turn && player2) {
-    //     currentPlayer = player2;
-    //   }
-    currentPlayer = isPlayer1Turn ? player1 : player2;
-    console.log("handlePlayerMove", currentPlayer);
-    if (currentPlayer) {
-      tile.textContent = currentPlayer.mark;
+const gameLogic = (() => {
+  const handlePlayerMove = (event) => {
+    tile = event.target;
+    console.log("gameLogic. players:", player1, player2);
+    if (!tile.textContent && !playedTiles.includes(tile)) {
+      currentPlayer = isPlayer1Turn ? player1 : player2;
+      console.log("gameLogic.", currentPlayer);
+      if (currentPlayer) {
+        tile.textContent = currentPlayer.mark;
+      }
+      console.log("gameLogic.", currentPlayer.mark);
+      tile.removeEventListener("click", handlePlayerMove);
+      playedTiles.push({ tile: tile.id, marker: currentPlayer.mark });
+      isPlayer1Turn = !isPlayer1Turn;
+      console.log(
+        "updateBoard playedTiles:",
+        playedTiles,
+        "turn:",
+        isPlayer1Turn
+      );
+      checkWin(currentPlayer);
     }
-    console.log("handlePlayerMove", currentPlayer.mark);
-    //   console.log("handlePlayerMove", tile, "Player", currentPlayerMark);
-    tile.removeEventListener("click", handlePlayerMove);
-    playedTiles.push({ tile: tile.id, marker: currentPlayer.mark });
-    isPlayer1Turn = !isPlayer1Turn;
-    console.log(
-      "updateBoard playedTiles:",
-      playedTiles,
-      "turn:",
-      isPlayer1Turn
-    );
-    checkWin(currentPlayer);
-  }
-};
+  };
 
-// const handlePlayerMove = (event) => {
-//   tile = event.target;
-//   if (!tile.textContent && !playedTiles.includes(tile)) {
-//     //This is because removeEventlListener was not working.
-//     if (isPlayer1Turn) {
-//       currentPlayerMark = player1.mark;
-//     } else {
-//       currentPlayerMark = player2.mark;
-//     }
-//     console.log("Player", currentPlayerMark);
-//     //   console.log("handlePlayerMove", tile, "Player", currentPlayerMark);
-//     tile.textContent = currentPlayerMark;
-//     tile.removeEventListener("click", handlePlayerMove);
-//     playedTiles.push({ tile: tile.id, marker: currentPlayerMark });
-//     isPlayer1Turn = !isPlayer1Turn;
-//     console.log(
-//       "updateBoard playedTiles:",
-//       playedTiles,
-//       "turn:",
-//       isPlayer1Turn
-//     );
-//     checkWin();
-//   }
-// };
+  const checkWin = (currentPlayer) => {
+    if (gameWon = winningConditions.some((combination) =>
+          combination.every((i) =>
+            playedTiles.some(
+              (tile) => tile.tile === i && tile.marker === currentPlayer.mark
+            )
+          )
+        ))
+       {
+        tiles.forEach((tile) => {
+          if (!tile.textContent && !playedTiles.includes(tile)) {
+            tile.removeEventListener("click", handlePlayerMove);
+          }
+        });
+        console.log("checkWin:", currentPlayer.mark, "wins");
+        displayText.displayWin(currentPlayer);
+      } else if (playedTiles.length === 9) {
+        console.log("checkWin is a Tie");
+        displayText.displayWin(cat);
+  };
+}
 
-const checkWin = (currentPlayer) => {
-  if (playedTiles.length === 9) {
-    //Do I move win declaration into displayText for congruency?
-    // catScore++;
-    // victory.style.display = "block";
-    // round.textContent = "This round goes to the TACOCAT.";
-    console.log("checkWin is a Tie");
-    displayText.displayWin(cat);
-    //return;
-  } else if (
-    (gameWon = winningConditions.some((combination) =>
-      combination.every((i) =>
-        playedTiles.some(
-          (tile) => tile.tile === i && tile.marker === currentPlayer.mark
-        )
-      )
-    ))
-  ) {
-    tiles.forEach((tile) => {
-        if (!tile.textContent && !playedTiles.includes(tile)) {
-          tile.removeEventListener("click", handlePlayerMove);
-        }
+  const playAgain = () => { //Vet the code below
+    playedTiles = [];
+    //playGameBtn.disabled = false;
+    document.querySelector('#player1 input[name="name"]').disabled = false;
+    const player1ChoiceInputs = document.querySelectorAll('#player1 input[name="choice"]');
+    player1ChoiceInputs.forEach(input => input.disabled = false);
+    document.querySelector('#player2 input[name="name"]').disabled = false;
+    const player2ChoiceInputs = document.querySelectorAll('#player2 input[name="choice"]');
+    player2ChoiceInputs.forEach(input => input.disabled = false);
+    tiles.forEach(tile => {
+        tile.textContent = ''; // Clear the tile text if necessary
+        tile.addEventListener('click', handlePlayerMove);
       });
-    console.log("checkWin:", currentPlayer.mark, "wins");
-    displayText.displayWin(currentPlayer);
-  }
-};
+  };
 
-//   const reset = () => {};
+  const reset = () => { //Vet the code below
+
+    playGameBtn.disabled = false;
+    document.querySelector('#player1 input[name="name"]').disabled = false;
+    const player1ChoiceInputs = document.querySelectorAll('#player1 input[name="choice"]');
+    player1ChoiceInputs.forEach(input => input.disabled = false);
+    document.querySelector('#player2 input[name="name"]').disabled = false;
+    const player2ChoiceInputs = document.querySelectorAll('#player2 input[name="choice"]');
+    player2ChoiceInputs.forEach(input => input.disabled = false);
+    tiles.forEach(tile => {
+        tile.textContent = ''; // Clear the tile text if necessary
+        tile.addEventListener('click', handlePlayerMove);
+});
+    player1.score = 0;
+    player2.score = 0;
+    catScore = 0;
+    victory.style.display = "none";
+  };
+  return { checkWin, handlePlayerMove, reset, playAgain };
+})();
 
 //eventListeners
 playGameBtn.addEventListener("click", displayText.displayNames);
 
 for (let tile of tiles) {
-  tile.addEventListener("click", handlePlayerMove);
+  tile.addEventListener("click", gameLogic.handlePlayerMove);
 }
+
+reset.addEventListener('click', gameLogic.reset);
+playAgain.addEventListener('click', gameLogic.playAgain);
